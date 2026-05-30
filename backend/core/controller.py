f54a54230aeebbd7852438f88c30d6bff0aae77f
mik07005 <intekhab071105@gmail.com>
2026-05-30 23:31:47 +0530

    Improved command intent detection

--- backend/core/controller.py
+++ backend/core/controller.py
@@ -1,27 +1,49 @@
 from rapidfuzz import fuzz
 
 from ai.ai_brain import generate_response
-
 from modules.automation.system_control import COMMANDS
+
+
+AUTOMATION_KEYWORDS = [
+    "open",
+    "launch",
+    "start",
+    "run",
+    "close",
+    "time"
+]
 
 
 def detect_command(user_input):
 
     user_input = user_input.lower()
 
+    # First check whether user is likely requesting automation
+
+    if not any(
+        keyword in user_input
+        for keyword in AUTOMATION_KEYWORDS
+    ):
+        return None
+
     best_match = None
     highest_score = 0
 
     for command in COMMANDS.keys():
 
-        score = fuzz.partial_ratio(user_input, command)
+        score = fuzz.partial_ratio(
+            user_input,
+            command
+        )
 
         if score > highest_score:
+
             highest_score = score
+
             best_match = command
 
-    # Threshold
     if highest_score >= 70:
+
         return best_match
 
     return None
